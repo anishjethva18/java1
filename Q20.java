c3a16655a3ac1ba4a92a7b250ded93ec579f4ef2
Anish Jethva <anish.jethva2006@gmail.com>
2026-02-10 13:28:56 +0530

    Update Q20.java

--- Q20.java
+++ Q20.java
@@ -1,45 +1,45 @@
-
 // BankAccount class with Deposit and Withdraw logic using Static values
-class BankAccount {
-    int accountNumber;
-    String accountHolderName;
-    double balance;
+class BankAccount1 {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
 
-    BankAccount(int a, String n, double b) {
-        accountNumber = a;
-        accountHolderName = n;
-        balance = b;
+    BankAccount1(int a, String n, double b) {
+        this.accountNumber = a;
+        this.accountHolderName = n;
+        this.balance = b;
     }
 
     void deposit(double amount) {
         balance = balance + amount;
-        System.out.println();
-        System.out.println("Deposited: " + amount);
+        System.out.println("\nDeposited: " + amount);
         System.out.println("Total Balance: " + balance);
     }
 
     void withdraw(double amount) {
         if (amount > balance) {
-            System.out.println("Error: Insufficient Balance for withdrawal of " + amount);
-        } 
-        else {
+            System.out.println("\nError: Insufficient Balance for withdrawal of " + amount);
+        } else {
             balance = balance - amount;
-            System.out.println("Withdrawn: " + amount);
+            System.out.println("\nWithdrawn: " + amount);
             System.out.println("New Balance: " + balance);
         }
+    }
+
+    void displayInfo() {
+        System.err.println("\nAccount Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Initial Balance: " + balance);
     }
 }
 
 public class Q20 {
     public static void main(String[] args) {
-        
-        BankAccount b = new BankAccount(101, "Rahul", 5000.0);
-        
-        System.out.println("Account Holder: " + b.accountHolderName);
-        System.out.println("Initial Balance: " + b.balance);
-        System.out.println();
+        BankAccount1 b = new BankAccount1(101, "Rahul", 5000.0);
+
+        b.displayInfo();
         System.out.println("-----------------------------------");
-        
+
         b.deposit(1500.0);
         b.withdraw(2000.0);
         b.withdraw(10000.0);
